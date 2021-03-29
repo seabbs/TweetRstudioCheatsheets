b928cc5a5cc16ebda56a44dd348d7298fc1f4e8c
seabbs <s.e.abbott12@gmail.com>
2021-03-29 10:37:19 +0100

    set defaults

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-gcloud functions deploy post_cheatsheet --region europe-west1 --project seabbs-personal  --memory=128MB --runtime python39 --trigger-http
+gcloud functions deploy post_cheatsheet  --memory=128MB --runtime python39 --trigger-http
