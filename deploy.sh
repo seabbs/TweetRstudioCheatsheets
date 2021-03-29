43c70f8912fa6ab61fc836256ea12a57ce1224a8
seabbs <s.e.abbott12@gmail.com>
2021-03-29 11:33:29 +0100

    fix indexing

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 gcloud functions deploy post_cheatsheet \
     --trigger-topic post-cheatsheet \
-     --memory=256MB \
-     --env-vars-file .env.yaml \
-     --runtime python39
+    --memory=256MB \
+    --env-vars-file .env.yaml \
+    --runtime python39
